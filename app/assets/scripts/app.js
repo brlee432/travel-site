d2366d313ecca681d39467d3c514d010b62e732b
Brandon <brlee432@gmail.com>
2017-12-07 19:53:34 -0500

    completed sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader.js';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "65%");
+var stickyHeader = new StickyHeader();
 
